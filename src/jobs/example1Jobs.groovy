04d11f61176aa06a1b4bea92f388b35cc314afaf
rovyas <rovyas@redhat.com>
2019-03-20 11:40:13 +0530

    Modified ex-1

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -1,27 +1,29 @@
-String basePath = 'example1'
-String repo = 'sheehan/gradle-example'
+String basePath = 'Spicegate-Hardware-GUI'
+String repo = 'git://git.host.prod.eng.bos.redhat.com/spicegate.git'
 
 folder(basePath) {
     description 'This example shows basic folder/job creation.'
 }
 
-job("$basePath/gradle-example-build") {
-    scm {
-        github repo
-    }
-    triggers {
-        scm 'H/5 * * * *'
-    }
-    steps {
-        gradle 'assemble'
-    }
-}
 
-job("$basePath/gradle-example-deploy") {
-    parameters {
-        stringParam 'host'
-    }
-    steps {
-        shell 'scp war file; restart...'
+def RHEL = ['7.x','8.x']
+def ARCH = ['x86_64','ppc-64']
+
+
+for (version in RHEL)
+{
+    for (arch in ARCH) {
+        job("$basePath/Hardware-spicegate-gui-$version-$arch") {
+            label('node1')
+            scm {
+                github repo
+            }
+            triggers {
+                scm 'H/5 * * * *'
+            }
+            steps {
+                shell 'scp war file; restart...'
+            }
+        }
     }
 }
